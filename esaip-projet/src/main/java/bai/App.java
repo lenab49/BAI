464a4e8684600712afc7e22c807f8fab44b974c6
lenaburon <lburon.ir2017@esaip.org>
2017-01-04 15:48:57 +0100

    Changement affichage

--- esaip-projet/src/main/java/bai/App.java
+++ esaip-projet/src/main/java/bai/App.java
@@ -6,10 +6,8 @@ package bai;
  */
 public class App {
 	public static void main(String[] args) {
-<<<<<<< HEAD:esaip-projet/src/main/java/bai/App.java
+
 		System.out.println("Lena 2 !");
-=======
-		System.out.println("Rem !");
->>>>>>> origin/master:esaip-projet/src/main/java/JPA_1/esaip_projet/App.java
+
 	}
 }
