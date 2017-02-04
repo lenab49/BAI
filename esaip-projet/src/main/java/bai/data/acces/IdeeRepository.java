1eceeefcd930e4d2a305eacd7ed10f5ee782d6f7
remdug <rem.dugue@gmail.com>
2017-02-04 13:03:23 +0100

    création de l'objet et ajout dans la base de donnée OK

--- esaip-projet/src/main/java/bai/data/acces/IdeeRepository.java
+++ esaip-projet/src/main/java/bai/data/acces/IdeeRepository.java
@@ -2,12 +2,8 @@ package bai.data.acces;
 
 import org.springframework.data.repository.CrudRepository;
 
-import antlr.collections.List;
 import bai.data.entite.Idee;
 
 public interface IdeeRepository extends CrudRepository<Idee, Long> {
-	
-
-	    Idee findByTitre(String titre);
-
+	Idee findByTitre(String titre);
 }
--- esaip-projet/src/main/java/bai/metier/idee/communication/IdeeController.java
+++ esaip-projet/src/main/java/bai/metier/idee/communication/IdeeController.java
@@ -1,36 +1,24 @@
 package bai.metier.idee.communication;
 
-import java.util.HashMap;
 import java.util.Map;
 
-import javax.persistence.EntityManager;
-import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
-
 import org.springframework.web.bind.annotation.ModelAttribute;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.support.SessionStatus;
-import org.springframework.web.servlet.ModelAndView;
 
+import bai.data.acces.IdeeRepository;
+import bai.data.entite.BDDAction;
 import bai.data.entite.Idee;
 import bai.metier.idee.service.IdeeService;
-import bai.metier.listeidee.dto.IdeeDto;
-import bai.data.entite.*;
-
 
 @Controller
 public class IdeeController {
 
-
 	final IdeeService ideeService;
+	@Autowired
+	private IdeeRepository repository;
 
 	@Autowired
 	public IdeeController(IdeeService ideeService) {
@@ -46,6 +34,8 @@ public class IdeeController {
 	public String addIdee(@ModelAttribute Idee idee) {
 		if (idee.getAuteur() != "" && idee.getTitre() != "" && idee.getContenu() != "") {
 			ideeService.setNewIdee(idee);
+			BDDAction bdd = new BDDAction();
+			bdd.save(repository, idee);
 		}
 		return "idee";
 	}
